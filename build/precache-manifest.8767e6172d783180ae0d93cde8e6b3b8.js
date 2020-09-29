745ad6bd400cdb40f21421d74f48055cb401d7a2
Marcelo Guilherme <marcelo@uniss.com.br>
2020-09-29 12:11:14 -0300

    Correcao do build (homepage: .)

---
+++ build/precache-manifest.8767e6172d783180ae0d93cde8e6b3b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5735a158546277a22805e7619d47b720",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ed263709f2cb57cc859d",
+    "url": "./static/css/main.91f685e7.chunk.css"
+  },
+  {
+    "revision": "ef0ea1dc059c9d138242",
+    "url": "./static/js/2.6402015e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.6402015e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed263709f2cb57cc859d",
+    "url": "./static/js/main.4e3c4e78.chunk.js"
+  },
+  {
+    "revision": "28bb356ee1ead8c4b64e",
+    "url": "./static/js/runtime-main.8297c8f4.js"
+  },
+  {
+    "revision": "1cf13f6c9469f862fe563ed235195e3d",
+    "url": "./static/media/logo_uniss_2020.1cf13f6c.svg"
+  }
+]);
